ed92d0f5094a613fa0b6e455972b056c8aaaaf39
Jonas Eriksson <jonaseriksson84@gmail.com>
2021-10-14 13:15:18 +0200

    gitignore

--- hackday/split/views.py
+++ hackday/split/views.py
@@ -1,24 +1,30 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from rest_framework import permissions
-from hackday.split.serializers import UserSerializer, GroupSerializer
+from rest_framework.decorators import api_view
+from rest_framework.exceptions import ParseError
+from rest_framework.parsers import FileUploadParser, FormParser, MultiPartParser
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
+from rest_framework.views import APIView
+from spleeter.separator import Separator
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-
-    queryset = User.objects.all().order_by("-date_joined")
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+@api_view()
+def hello_world(request):
+    separator = Separator("spleeter:2stems")
+    separator.separate_to_file("./missli.mp3", "./output/")
+    print("hej")
+    return Response({"message": "Hello, worldz!"})
 
 
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
+class UploadView(APIView):
+    # parser_class = (FileUploadParser,)
+    parser_classes = (FormParser, MultiPartParser)
 
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    def put(self, request, format=None):
+        if "file" not in request.data:
+            raise ParseError("Empty content")
+
+        f = request.data["file"]
+
+        # mymodel.my_file_field.save(f.name, f, save=True)
+        print(f)
+        return Response(status=HTTP_201_CREATED)
--- hackday/urls.py
+++ hackday/urls.py
@@ -1,14 +1,14 @@
-from django.urls import include, path
+from django.conf.urls import url
+from rest_framework.urls import path
 from rest_framework import routers
 from hackday.split import views
 
 router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet)
-router.register(r"groups", views.GroupViewSet)
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path("", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    url(r"^split/$", views.hello_world),
+    path("upload/", views.UploadView.as_view(), name="upload"),
 ]
